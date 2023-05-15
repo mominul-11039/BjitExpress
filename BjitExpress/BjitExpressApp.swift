c9a64d7ee3b5d764e49a1055cfa48cb2db68a41d
MOMINUL ISLAM <islam.mominul@bjitgroup.com>
2023-05-15 12:01:01 +0600

    Project Created

---
+++ BjitExpress/BjitExpressApp.swift
@@ -0,0 +1,20 @@
+//
+//  BjitExpressApp.swift
+//  BjitExpress
+//
+//  Created by Md. Mominul Islam on 15/5/23.
+//
+
+import SwiftUI
+
+@main
+struct BjitExpressApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
